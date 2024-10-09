b1290b921bacb1c482665581eb2f0076bfc7329d
orsoli <fljcoli@gmail.com>
2024-10-10 00:36:46 +0200

    Completed Bonus 1

--- classes/Movies.php
+++ classes/Movies.php
@@ -8,12 +8,12 @@ require_once __DIR__."/Genres.php";
  class Movie{
     // AProperties
     public string $title;
-    public string $genre;
+    public array $genre;
     public int $year;
     public float $rate;
 
     // Constructor method
-    public function __construct(string $title, string $genre,int $year,float $rate)
+    public function __construct(string $title, array $genre,int $year,float $rate)
     {
         $this->title=$title;
         $this->genre=$genre;
@@ -25,9 +25,9 @@ require_once __DIR__."/Genres.php";
 
  $movies = [
         // Create instances
-        new Movie("Inception", "Sci-Fi",2010, 8.8 ),
-        new Movie("The Godfather", "Crime",1972, 9.2),
-        new Movie("The Dark Knight", "Action",2008, 9.0),
-        new Movie("Forrest Gump", "Drama",1994, 8.8),
-        new Movie("The Matrix", "Sci-Fi",1999, 8.7),
+        new Movie("Inception", ["Sci-Fi","Action"],2010, 8.8 ),
+        new Movie("The Godfather", ["Crime"],1972, 9.2),
+        new Movie("The Dark Knight", ["Action"],2008, 9.0),
+        new Movie("Forrest Gump", ["Drama"],1994, 8.8),
+        new Movie("The Matrix", ["Sci-Fi"],1999, 8.7),
  ];
